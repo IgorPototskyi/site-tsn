1dfba3e31dbf5448dea7df84b433ff617be663bf
IgorPototskyi <igorekpototsky@gmail.com>
2017-07-17 20:11:20 +0300

    second version of video

--- app/js/common.js
+++ app/js/common.js
@@ -66,12 +66,12 @@ $(function() {
         soc.hide(300);
         $(this).toggleClass('top-nav__search--big');
         
-    })
+    });
 
     $('.top-nav__share-btn').on('click', function() {
         topNavSearch.removeClass('top-nav__search--big');
         soc.toggle(300);
-    })
+    });
 
     // ----- News header -----
 
@@ -79,20 +79,42 @@ $(function() {
     newsLinks.on('click', function() {
         newsLinks.removeClass('active');
         $(this).addClass('active');
-    })
+    });
 
     // ----- Video -----
-    
-    var newsItemVideo = $('.news__item-video');
-    $('.news__item-share').on('click', function(e) {
-        e.preventDefault(); 
-        newsItemVideo.css('left', topNav.position().left + 20);
-        $(this).parent().toggleClass('news__item-cont--shared');
-        $(this).siblings('.news__item-video').toggleClass('active');
 
-        
+    $('.news__item-shade').clone().appendTo('.news__item-cont--video');
+    $('.news__icon-play').clone().appendTo('.news__item-cont--video');
+    $('.news__icon-share').clone().appendTo('.news__item-cont--video');
+
+    var newsVideo = $('.news__video');
+    var newsItemCont = $('.news__item-cont');
+
+    newsVideo.css('left', topNav.position().left + 20);
+    
+    $('.news__icon-share').on('click', function(e) {
+        e.preventDefault(); 
+
+        if (!$(this).parent().hasClass('news__item-cont--shared')) {
+            newsVideo.removeClass('active');
+            newsItemCont.removeClass('news__item-cont--shared');
+            $(this).parent().addClass('news__item-cont--shared');
+
+            newsVideo.children('.news__item-img').remove();
+            $(this).siblings('.news__item-img').clone().prependTo(newsVideo);
+            newsVideo.addClass('active');
+        } else {
+            newsVideo.removeClass('active');
+            newsItemCont.removeClass('news__item-cont--shared');
+        }
+
         return false; 
-    })
+    });
+
+    $('.news__video-close').on('click', function(e) {
+        newsVideo.removeClass('active');
+        newsItemCont.removeClass('news__item-cont--shared');
+    });
 
 
 });
